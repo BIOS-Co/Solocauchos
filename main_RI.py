25062b110760219825d435efc2cc23172b03b379
Rafael Mejia <rmejiaz@unal.edu.co>
2022-12-13 11:14:09 -0500

    Corrected mistakes

--- main_RI.py
+++ main_RI.py
@@ -13,7 +13,7 @@ import time
 
 def main():
 
-    conn = create_server_connection('localhost', 'admin', 'password', 'SolocauchosV3')
+    conn = create_server_connection('localhost', 'root', 'pamd101216', 'iot')
 
     state_labels = {
     0: "Sensor Desconectado",
@@ -28,92 +28,91 @@ def main():
     model_state = pickle.load(open('modelo_estado.sav', 'rb'))
 
 
-    while(True):
 
-         # Get the database head table
+    # Get the database head table
 
-        query = "select * from pulsos_iot_vibration_encabezado;"
-        col_names = get_col_names(conn, 'pulsos_iot_vibration_encabezado')
-        encabezado = create_df(conn, query, col_names)
-
-
-        # Do the query to obtain the latest signal
-
-        minuto = encabezado['cod_pulso'].iloc[-1] - 1
-        signals = get_signals(minuto, conn)
+    query = "select * from pulsos_iot_vibration_encabezado;"
+    col_names = get_col_names(conn, 'pulsos_iot_vibration_encabezado')
+    encabezado = create_df(conn, query, col_names)
 
 
-        # Do the state prediction using the model
+    # Do the query to obtain the latest signal
 
-        # Preprocessing:
-
-        X_rms = np.sqrt(np.mean(signals[:,-100:,:]**2, axis = 1))
-
-        # Prediction:
-
-        last_state_pred = model_state.predict(X_rms)[0]
-
-        first_state_pred = model_state.predict(np.sqrt(np.mean(signals[:,:100,:]**2, axis = 1)))
-
-        # Do the anomaly detection according to the signal type:
+    minuto = encabezado['cod_pulso'].iloc[-1] - 1
+    signals = get_signals(minuto, conn)
 
 
-        if last_state_pred == first_state_pred:
-            if last_state_pred == 0:
-                anomaly_pred = 0
-            
-            elif last_state_pred == 1:
-                anomalies = np.sum(np.abs(signals)>0.03)
-                if anomalies > 300:
-                    anomaly_pred = 1
-                else:
-                    anomaly_pred = 0
-            
-            elif last_state_pred == 2:
-                anomalies = np.sum(np.abs(signals)>0.6)
-                if anomalies > 50:
-                    anomaly_pred = 1
-                else:
-                    anonaly_pred = 0    
+    # Do the state prediction using the model
 
-        else:
+    # Preprocessing:
+
+    X_rms = np.sqrt(np.mean(signals[:,-100:,:]**2, axis = 1))
+
+    # Prediction:
+
+    last_state_pred = model_state.predict(X_rms)[0]
+
+    first_state_pred = model_state.predict(np.sqrt(np.mean(signals[:,:100,:]**2, axis = 1)))
+
+    # Do the anomaly detection according to the signal type:
+
+
+    if last_state_pred == first_state_pred:
+        if last_state_pred == 0:
             anomaly_pred = 0
-
-        # Display results:
-
-        print(state_labels[last_state_pred])
-        if anomaly_pred == 0:
-            print("Señal sin anomalía")
         
-        else:
-            print("Señal con anomalía")
-       
-        plt.plot(signals[0])
-        plt.show()
+        elif last_state_pred == 1:
+            anomalies = np.sum(np.abs(signals)>0.03)
+            if anomalies > 300:
+                anomaly_pred = 1
+            else:
+                anomaly_pred = 0
+        
+        elif last_state_pred == 2:
+            anomalies = np.sum(np.abs(signals)>0.6)
+            if anomalies > 50:
+                anomaly_pred = 1
+            else:
+                anonaly_pred = 0    
 
-        # Insert the prediction result in the database:
+    else:
+        anomaly_pred = 0
+
+    # Display results:
+
+    print(state_labels[last_state_pred])
+    if anomaly_pred == 0:
+        print("Señal sin anomalía")
+    
+    else:
+        print("Señal con anomalía")
+    
+    plt.plot(signals[0])
+    plt.show()
+
+    # Insert the prediction result in the database:
 
 
-        query = f"""
-        UPDATE pulsos_iot_vibration_encabezado
-        SET resultado_ia = {last_state_pred},
-        WHERE cod_pulso = {minuto};
-        """
+    query = f"""
+    UPDATE pulsos_iot_vibration_encabezado
+    SET resultado_ia = {last_state_pred},
+    WHERE cod_pulso = {minuto};
+    """
 
-        execute_query(conn, query)
-        print(f"Pulso analizado: {minuto}")
+    execute_query(conn, query)
+    print(f"Pulso analizado: {minuto}")
 
 
-        query_anomalia = f"""
-        UPDATE pulsos_iot_vibration_encabezado
-        SET anomalia = {anomaly_pred},
-        WHERE cod_pulso = {minuto};
-        """
+    query_anomalia = f"""
+    UPDATE pulsos_iot_vibration_encabezado
+    SET anomalia = {anomaly_pred},
+    WHERE cod_pulso = {minuto};
+    """
 
-        execute_query(conn, query_anomalia)
-        print(f"Pulso analizado: {minuto}")
+    execute_query(conn, query_anomalia)
+    print(f"Pulso analizado: {minuto}")
 
-        time.sleep(90)
+        
 
 if __name__ == "__main__":
     main()
